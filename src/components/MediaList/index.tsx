4019e6956f816e36116fb483db583c62a0f31c92
Seadet <seadet.hesenzade2000@gmail.com>
2024-07-20 20:40:42 +0400

    Bookmark page uodate

--- src/components/MediaList/index.tsx
+++ src/components/MediaList/index.tsx
@@ -1,33 +1,40 @@
-import React from 'react'
-import style from './style.module.scss';
-import {Movie} from '../../interfaces/Movie';
-import MediaCard from '../MediaCard';
-import { useBookmarkContext } from '../../contexts/BookmarkContext';
-import { useSearchContext } from '../../contexts/SearchContext';
+import React from "react";
+import style from "./style.module.scss";
+import { Movie } from "../../interfaces/Movie";
+import MediaCard from "../MediaCard";
+import { useBookmarkContext } from "../../contexts/BookmarkContext";
+import { useSearchContext } from "../../contexts/SearchContext";
 
-interface FilterData{
-  FilterFunction: (
-    movies: Movie[],
-  )=>Movie[],
-  id:string,
-  heading:string,
+interface FilterData {
+  FilterFunction: (movies: Movie[]) => Movie[];
+  id: string;
+  heading: string;
+  isBookmarkPage?: boolean;
 }
 
-const MediaList:React.FC<FilterData>= ({FilterFunction,id,heading}) => {
-  const { movies} = useBookmarkContext();
-  const { searchQuery} = useSearchContext();
-  const filteredData: Movie[] =FilterFunction(movies); 
-  const pageTitle=searchQuery ? `Found ${filteredData.length} results for' ${searchQuery}'` : heading
+const MediaList: React.FC<FilterData> = ({
+  FilterFunction,
+  id,
+  heading,
+  isBookmarkPage,
+}) => {
+  const { movies } = useBookmarkContext();
+  const { searchQuery } = useSearchContext();
+  const filteredData: Movie[] = FilterFunction(movies);
+  const pageTitle = searchQuery
+    ? `Found ${filteredData.length} results for' ${searchQuery}'`
+    : heading;
+  if (isBookmarkPage && !searchQuery && !filteredData.length) return;
   return (
     <section id={`${style[id]}`}>
-        <h2 className={style.title}>{pageTitle}</h2>
-        <div className={style.mediaList}>
-        {filteredData.map((data)=>{
-            return <MediaCard key={data.title} FilteredMedia={data}/>
-          })}
-        </div>
+      <h2 className={style.title}>{pageTitle}</h2>
+      <div className={style.mediaList}>
+        {filteredData.map((data) => {
+          return <MediaCard key={data.title} FilteredMedia={data} />;
+        })}
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default MediaList
+export default MediaList;
--- src/pages/Bookmarks.tsx
+++ src/pages/Bookmarks.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../styles/_variables.scss";
 import {
   filterBookmarkedMovies,
   filterBookmarkedSeries,
@@ -7,30 +8,35 @@ import {
 } from "../utils/dataFilters";
 import MediaList from "../components/MediaList";
 import { useSearchContext } from "../contexts/SearchContext";
-import { Movie } from "../interfaces/Movie";
-
+import { useBookmarkContext } from "../contexts/BookmarkContext";
+import "./Bookmark.scss";
 const Bookmarks: React.FC = () => {
   const { searchQuery } = useSearchContext();
+  const { movies } = useBookmarkContext();
+  const bookmarkedMedia = filterBookmarkedMedia(movies);
   const filterFunction = searchQuery
-    ? (movies: Movie[]) =>
-        filterSearchResults(filterBookmarkedMedia(movies), searchQuery)
+    ? () => filterSearchResults(bookmarkedMedia, searchQuery)
     : filterBookmarkedMovies;
 
-  return (
+  return bookmarkedMedia.length > 0 || searchQuery ? (
     <>
       <MediaList
         FilterFunction={filterFunction}
         id="bookmarkedMovies"
         heading="Bookmarked Movies"
+        isBookmarkPage
       />
       {!searchQuery && (
         <MediaList
           FilterFunction={filterBookmarkedSeries}
           id="bookmarkedSeries"
           heading="Bookmarked TV Series"
+          isBookmarkPage
         />
       )}
     </>
+  ) : (
+    <h2 className="title">You have no bookmarked show</h2>
   );
 };
 
