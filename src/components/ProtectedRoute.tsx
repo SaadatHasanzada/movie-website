85a55b1157978c388158f1aadbbd75fe96964b15
Seadet <seadet.hesenzade2000@gmail.com>
2025-01-18 13:50:36 +0400

    Add loading state handling to ProtectedRoute component

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -2,20 +2,22 @@ import { FC, ReactElement } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 
 import MainLayout from "@/layouts/MainLayout";
+import PageLoader from "./PageLoader";
 import { ROUTES } from "@/constants";
+import { useAuth } from "@/features/auth/hooks/AuthContext";
 
 interface ProtectedRouteProps {
   children: ReactElement;
-  isAuthenticated: boolean;
 }
 
-export const ProtectedRoute: FC<ProtectedRouteProps> = ({
-  children,
-  isAuthenticated
-}) => {
+export const ProtectedRoute: FC<ProtectedRouteProps> = ({ children }) => {
   const location = useLocation();
+  const { isAuthenticated, isLoading } = useAuth();
+  if (isLoading) {
+    return <PageLoader />;
+  }
 
-  if (!isAuthenticated) {
+  if (!isLoading && !isAuthenticated) {
     return <Navigate to={ROUTES.LANDING} state={{ from: location }} replace />;
   }
 
