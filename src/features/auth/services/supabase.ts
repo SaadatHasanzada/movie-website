b59d9654d5bc521187d5c2c2afa1f0577948d0f5
Seadet <seadet.hesenzade2000@gmail.com>
2025-01-30 18:51:55 +0400

    Add profile photo upload and deletion functionality to userService

--- src/features/auth/services/supabase.ts
+++ src/features/auth/services/supabase.ts
@@ -1,8 +1,14 @@
 import { Session, createClient } from "@supabase/supabase-js";
 
+import { User } from "@supabase/supabase-js";
+
 interface AuthCredentials {
   email: string;
   password: string;
+}
+interface UpdateProfilePhotoParams {
+  file: File;
+  user: User;
 }
 
 export const supabase = createClient(
@@ -51,9 +57,11 @@ export const authService = {
     } = await supabase.auth.getSession();
     return !!session;
   },
-  onAuthStateChanged: (callback: (session: Session | null) => void) => {
-    return supabase.auth.onAuthStateChange((_, session) => {
-      callback(session);
+  onAuthStateChanged: (
+    callback: (session: Session | null, event?: string) => void
+  ) => {
+    return supabase.auth.onAuthStateChange((event, session) => {
+      callback(session, event);
     }).data.subscription;
   }
 };
@@ -65,6 +73,68 @@ export const userService = {
         data: { user }
       } = await supabase.auth.getUser();
       return user;
+    } catch (err) {
+      handleError(err);
+    }
+  },
+  uploadProfilePhoto: async ({ file, user }: UpdateProfilePhotoParams) => {
+    try {
+      // Generate a unique file path
+      const fileExt = file.name.split(".").pop();
+      const fileName = `${user.id}_${Date.now()}.${fileExt}`;
+      const filePath = `public/${fileName}`;
+
+      // Upload the file to Supabase storage
+      const { error: uploadError } = await supabase.storage
+        .from("profile-photos")
+        .upload(filePath, file, {
+          cacheControl: "3600",
+          upsert: true // Enable upsert to replace existing files
+        });
+
+      if (uploadError) throw uploadError;
+
+      // Get the public URL for the uploaded file
+
+      const {
+        data: { publicUrl }
+      } = supabase.storage.from("profile-photos").getPublicUrl(filePath);
+
+      return publicUrl;
+    } catch (err) {
+      handleError(err);
+    }
+  },
+  deleteProfilePhoto: async (user: User) => {
+    try {
+      const filePath = user.user_metadata.image;
+      if (!filePath) return;
+
+      const { error: deleteError } = await supabase.storage
+        .from("profile-photos")
+        .remove([filePath]);
+
+      if (deleteError) throw deleteError;
+
+      const { error: updateError } = await supabase.auth.updateUser({
+        data: {
+          image: null
+        }
+      });
+      if (updateError) throw updateError;
+      return true;
+    } catch (err) {
+      handleError(err);
+      throw err;
+    }
+  },
+  updateUser: async (profile: Partial<User>) => {
+    try {
+      const { data, error } = await supabase.auth.updateUser(profile);
+
+      if (error) throw error;
+      console.log(data);
+      return data;
     } catch (err) {
       handleError(err);
     }
