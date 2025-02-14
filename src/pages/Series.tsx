52f9fb35f63320a0c5cd2b3785c9129ef0bf9edb
Seadet <seadet.hesenzade2000@gmail.com>
2025-02-14 06:43:46 +0400

    Refactor Series component to use Media interface for movie data

--- src/pages/Series.tsx
+++ src/pages/Series.tsx
@@ -1,16 +1,23 @@
-import React from 'react'
-import MediaList from '../components/MediaList';
-import { filterSeries,filterSearchResults } from '../utils/dataFilters';
-import { useSearchContext } from '../contexts/SearchContext';
-import { Movie } from '../interfaces/Movie';
-const Series:React.FC = () => {
-  const {searchQuery}=useSearchContext();
-  const filterFunction = searchQuery
-  ? (movies: Movie[]) => filterSearchResults(filterSeries(movies), searchQuery)
-  : filterSeries;
-  return (
-    <MediaList FilterFunction={filterFunction} id='series' heading='TV Series'/>
-  )
-}
+import { filterSearchResults, filterSeries } from "../utils/dataFilters";
 
-export default Series
+import MediaList from "../components/MediaList";
+import { Movie } from "../interfaces/Media";
+import React from "react";
+import { useSearchContext } from "../contexts/SearchContext";
+
+const Series: React.FC = () => {
+  const { searchQuery } = useSearchContext();
+  const filterFunction = searchQuery
+    ? (movies: Movie[]) =>
+        filterSearchResults(filterSeries(movies), searchQuery)
+    : filterSeries;
+  return (
+    <MediaList
+      FilterFunction={filterFunction}
+      id="series"
+      heading="TV Series"
+    />
+  );
+};
+
+export default Series;
