a6670299ee43dcab461f4a946c1a33ee3023f8ea
Seadet <seadet.hesenzade2000@gmail.com>
2025-02-14 06:43:22 +0400

    Add Media interface to define media properties and structure

---
+++ src/interfaces/Media.ts
@@ -0,0 +1,40 @@
+import { category } from "../types/category";
+
+interface Thumbnail {
+  regular: {
+    small: string;
+    medium: string;
+    large: string;
+  };
+  trending?: {
+    small: string;
+    large: string;
+  };
+}
+
+export interface Media {
+  id: number;
+  adult: boolean;
+  media_type: string;
+  backdrop_path: string;
+  overview: string;
+  poster_path: string;
+  genre_ids: number[];
+  original_language: string;
+  popularity: number;
+  vote_average: number;
+  vote_count: number;
+  title?: string;
+  original_title?: string;
+  release_date?: string;
+  video?: boolean;
+  first_air_date?: string;
+  name?: string;
+  origin_country?: string[];
+  original_name?: string;
+}
+// export interface Movie extends Media {
+//   //   isBookmarked: boolean;
+//   //   isTrending: boolean;
+// }
+// export interface TvSeries extends Media {}
