a571fe05ff083ec6a2546ad602f6e8b5e3e5afbd
Seadet <seadet.hesenzade2000@gmail.com>
2025-02-14 06:43:50 +0400

    Refactor dataFilters to use Media interface for filtering functions

--- src/utils/dataFilters.ts
+++ src/utils/dataFilters.ts
@@ -1,27 +1,36 @@
-import {Movie} from '../interfaces/Movie';
+import { Movie } from "../interfaces/Media";
 
-export const filterTrending=(mediaData:Movie[])=>{
-    return mediaData.filter((item: Movie) => item.thumbnail.trending)
-}
-export const filterRecommended=(mediaData:Movie[])=>{
-    return mediaData.filter((item: Movie) => !item.thumbnail.trending)
-}
-export const filterMovies=(mediaData:Movie[])=>{
-    return mediaData.filter((item: Movie) => item.category==='Movie')
-}
-export const filterSeries=(mediaData:Movie[])=>{
-    return mediaData.filter((item: Movie) => item.category==='TV Series')
-}
-export const filterBookmarkedMovies=(mediaData:Movie[])=>{
-    return mediaData.filter((item: Movie) => item.category==='Movie' && item.isBookmarked )
-}
-export const filterBookmarkedSeries=(mediaData:Movie[])=>{
-    return mediaData.filter((item: Movie) => item.category==='TV Series' && item.isBookmarked)
-}
-export const filterBookmarkedMedia=(mediaData:Movie[])=>{
-    return mediaData.filter((item: Movie) => item.isBookmarked)
-}
+export const filterTrending = (mediaData: Movie[]) => {
+  return mediaData.filter((item: Movie) => item.thumbnail.trending);
+};
+export const filterRecommended = (mediaData: Movie[]) => {
+  return mediaData.filter((item: Movie) => !item.thumbnail.trending);
+};
+export const filterMovies = (mediaData: Movie[]) => {
+  return mediaData.filter((item: Movie) => item.category === "Movie");
+};
+export const filterSeries = (mediaData: Movie[]) => {
+  return mediaData.filter((item: Movie) => item.category === "TV Series");
+};
+export const filterBookmarkedMovies = (mediaData: Movie[]) => {
+  return mediaData.filter(
+    (item: Movie) => item.category === "Movie" && item.isBookmarked
+  );
+};
+export const filterBookmarkedSeries = (mediaData: Movie[]) => {
+  return mediaData.filter(
+    (item: Movie) => item.category === "TV Series" && item.isBookmarked
+  );
+};
+export const filterBookmarkedMedia = (mediaData: Movie[]) => {
+  return mediaData.filter((item: Movie) => item.isBookmarked);
+};
 
-export const filterSearchResults = (movies: Movie[], query: string): Movie[] => {
-    return movies.filter(movie => movie.title.toLowerCase().includes(query.toLowerCase()));
-  };
+export const filterSearchResults = (
+  movies: Movie[],
+  query: string
+): Movie[] => {
+  return movies.filter((movie) =>
+    movie.title.toLowerCase().includes(query.toLowerCase())
+  );
+};
