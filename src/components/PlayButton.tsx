8a85d6f2437f47b56e5def7e78d97157f9d0d3e5
Seadet <seadet.hesenzade2000@gmail.com>
2025-02-16 05:35:53 +0400

    Enhance PlayButton component with click handling and state management

--- src/components/PlayButton.tsx
+++ src/components/PlayButton.tsx
@@ -2,17 +2,31 @@ import React from "react";
 
 interface PlayButtonProps {
   isHovered: boolean;
+  onClick: () => void;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
-const PlayButton: React.FC<PlayButtonProps> = ({ isHovered }) => {
+const PlayButton: React.FC<PlayButtonProps> = ({
+  isHovered,
+  onClick,
+  setIsOpen
+}) => {
+  const handleClick = () => {
+    setIsOpen(true);
+    onClick();
+  };
   return (
     <div
-      className={`animate-pulse-white text-lg font-medium absolute bottom-6 right-6 rounded-[50%] bg-white/25 z-30 scale-[1] shadow-[0 0 0 0 rgba(255, 255, 255, 0.25)]   ${
+      onClick={handleClick}
+      className={`z-50 animate-pulse-white text-lg font-medium absolute bottom-4 right-4 ms:bottom-6 ms:right-6 rounded-[50%] bg-white/25 scale-[1] shadow-[0 0 0 0 rgba(255, 255, 255, 0.25)]   ${
         isHovered ? "block" : "hidden"
       } `}
     >
-      <svg width="36" height="36" xmlns="http://www.w3.org/2000/svg">
+      <svg
+        className="w-[28px] h-[28px]  ms:w-9 ms:h-9"
+        xmlns="http://www.w3.org/2000/svg"
+      >
         <path
-          transform="scale(1.2)"
+          className="scale-[0.9] ms:scale-[1.2]"
           d="M15 0C6.713 0 0 6.713 0 15c0 8.288 6.713 15 15 15 8.288 0 15-6.712 15-15 0-8.287-6.712-15-15-15Zm-3 21V8l9 6.5-9 6.5Z"
           fill="#FFF"
         />
