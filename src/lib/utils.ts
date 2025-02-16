84041d200853be0492469f83ae2ec1333eaacc22
Seadet <seadet.hesenzade2000@gmail.com>
2025-02-16 05:36:22 +0400

    Add findTrailer utility function to retrieve official trailers from video list

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,22 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
-
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { Video } from "@/interfaces/Media";
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+export const findTrailer = (videos: Video[]): Video | null => {
+  if (!videos?.length) return null;
+  const officialTrailer = videos.find(
+    (video) =>
+      video.type === "Trailer" &&
+      video.official === true &&
+      video.site === "YouTube"
+  );
+  if (officialTrailer) return officialTrailer;
+
+  return (
+    videos.find(
+      (video) => video.type === "Teaser" && video.site === "YouTube"
+    ) || null
+  );
+};
