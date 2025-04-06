205a72fc62775b578b72085fcd0612a1810753e0
Seadet <seadet.hesenzade2000@gmail.com>
2025-04-07 02:18:14 +0400

    Refactor Trending component to simplify data merging using joinMedia function

--- src/components/Trending.tsx
+++ src/components/Trending.tsx
@@ -3,19 +3,17 @@ import MediaSlider from "./MediaSlider";
 import React from "react";
 import TrendingCard from "./TrendingCard";
 import { getTrendingMedia } from "@/api/tmdb";
+import { joinMedia } from "@/lib/utils";
 import { useAsyncService } from "@/hooks/useAsyncService";
 
 // import { useBookmarkContext } from "../contexts/BookmarkContext";
 
 // import { filterTrending } from "../utils/dataFilters";
 
-
-
-
 const Trending: React.FC = () => {
   // const { movies } = useBookmarkContext();
   // const trendingData: Movie[] = filterTrending(movies);
-  const trendingData: Media[] = [];
+  let trendingData: Media[] = [];
   const { data: trendingMedia, loading: isTrendingMediaLoading } =
     useAsyncService(getTrendingMedia);
 
@@ -23,13 +21,7 @@ const Trending: React.FC = () => {
     const [movies, tvShows] = trendingMedia;
     const movieResults = movies.data.results;
     const tvResults = tvShows.data.results;
-
-    const maxLength = Math.max(movieResults.length, tvResults.length);
-
-    for (let i = 0; i < maxLength; i++) {
-      if (i < movieResults.length) trendingData.push(movieResults[i]);
-      if (i < tvResults.length) trendingData.push(tvResults[i]);
-    }
+    trendingData = joinMedia(movieResults, tvResults);
   }
   return (
     <MediaSlider title="Trending">
