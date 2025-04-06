a12494f819d7ad3e5b918786d0faa2208a137e57
Seadet <seadet.hesenzade2000@gmail.com>
2025-04-07 02:19:01 +0400

    Refactor Home component to integrate recommended media fetching and improve media handling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,39 @@
 import { filterRecommended, filterSearchResults } from "../utils/dataFilters";
 
+import { Media } from "@/interfaces/Media";
 import MediaList from "../components/MediaList";
-import { Movie } from "../interfaces/Media";
 import React from "react";
 import Trending from "../components/Trending";
+import { getRecommendedMedia } from "@/api/tmdb";
+import { joinMedia } from "@/lib/utils";
+import { useAsyncService } from "@/hooks/useAsyncService";
 import { useSearchContext } from "../contexts/SearchContext";
 
 const Home: React.FC = () => {
+  const { data: recommendedMedia, loading: isRecommendedMediaLoading } =
+    useAsyncService(getRecommendedMedia);
+  let recommendedData: Media[] = [];
+
+  // why you dont use state here instead of normal variable
+
   const { searchQuery } = useSearchContext();
-  const filterFunction = searchQuery
-    ? (movies: Movie[]) => filterSearchResults(movies, searchQuery)
-    : filterRecommended;
+  // const filterFunction = searchQuery
+  //   ? (movies: Media[]) => filterSearchResults(movies, searchQuery)
+  //   : filterRecommended;
+
+  if (recommendedMedia) {
+    recommendedData = joinMedia(recommendedMedia);
+  }
 
   return (
     <>
       {!searchQuery && <Trending />}
       <MediaList
-        FilterFunction={filterFunction}
+        // FilterFunction={filterFunction}
         id="recommended"
         heading="Recommended for you"
+        media={recommendedData}
+        isMediaLoading={isRecommendedMediaLoading}
       />
     </>
   );
