fe185538bfdc48dec4f668f69b44e189225fe2ba
Seadet <seadet.hesenzade2000@gmail.com>
2025-04-10 21:06:53 +0400

    Refactor Series component to manage TV series data with state and enhance rendering logic

--- src/pages/Series.tsx
+++ src/pages/Series.tsx
@@ -1,23 +1,56 @@
 import { filterSearchResults, filterSeries } from "../utils/dataFilters";
+import { useEffect, useState } from "react";
 
+import { Media } from "@/interfaces/Media";
 import MediaList from "../components/MediaList";
-import { Movie } from "../interfaces/Media";
+import MediaSlider from "@/components/MediaSlider";
 import React from "react";
+import TrendingCard from "@/components/TrendingCard";
+import { addMediaType } from "@/lib/utils";
+import { getAllTvShows } from "@/api/tmdb";
+import { useAsyncService } from "@/hooks/useAsyncService";
 import { useSearchContext } from "../contexts/SearchContext";
 
 const Series: React.FC = () => {
   const { searchQuery } = useSearchContext();
-  const filterFunction = searchQuery
-    ? (movies: Movie[]) =>
-        filterSearchResults(filterSeries(movies), searchQuery)
-    : filterSeries;
-  return (
-    <MediaList
-      FilterFunction={filterFunction}
-      id="series"
-      heading="TV Series"
-    />
-  );
+
+  const { data: tvSeries, loading: isTvSeriesLoading } =
+    useAsyncService(getAllTvShows);
+  const [tvSeriesData, setTvSeriesData] = useState<
+    {
+      data: Media[];
+      title: string;
+    }[]
+  >([]);
+
+  console.log(tvSeries);
+
+  useEffect(() => {
+    if (tvSeries) {
+      setTvSeriesData([
+        { data: tvSeries[3].data?.results, title: "Airing Today" },
+        { data: tvSeries[0].data?.results, title: "Popular Tv Series" },
+        { data: tvSeries[1].data?.results, title: "Top Rated Tv Series" },
+        { data: tvSeries[2].data?.results, title: "On The Air" }
+      ]);
+    }
+  }, [tvSeries]);
+
+  // const filterFunction = searchQuery
+  //   ? (movies: Movie[]) =>
+  //       filterSearchResults(filterSeries(movies), searchQuery)
+  //   : filterSeries;
+  return tvSeriesData.map((seriesList) => (
+    <MediaSlider key={seriesList.title} title={seriesList.title}>
+      {addMediaType(seriesList.data, "tv")?.map((media) => (
+        <TrendingCard
+          key={media.id}
+          TrendingMedia={media}
+          isTrendingMediaLoading={isTvSeriesLoading}
+        />
+      )) || []}
+    </MediaSlider>
+  ));
 };
 
 export default Series;
