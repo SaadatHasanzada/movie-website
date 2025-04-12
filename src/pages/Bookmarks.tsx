99a6284465475c44914ceea00ccdce97a79b58ec
Seadet <seadet.hesenzade2000@gmail.com>
2025-04-12 16:23:09 +0400

    Remove commented-out code and replace return statement with a placeholder in Bookmarks component

--- src/pages/Bookmarks.tsx
+++ src/pages/Bookmarks.tsx
@@ -15,32 +15,33 @@ import { useSearchContext } from "../contexts/SearchContext";
 
 const Bookmarks: React.FC = () => {
   const { searchQuery } = useSearchContext();
-  const { movies } = useBookmarkContext();
-  const bookmarkedMedia = filterBookmarkedMedia(movies);
-  const filterFunction = searchQuery
-    ? () => filterSearchResults(bookmarkedMedia, searchQuery)
-    : filterBookmarkedMovies;
+  // const { movies } = useBookmarkContext();
+  // const bookmarkedMedia = filterBookmarkedMedia(movies);
+  // const filterFunction = searchQuery
+  //   ? () => filterSearchResults(bookmarkedMedia, searchQuery)
+  //   : filterBookmarkedMovies;
 
-  return bookmarkedMedia.length > 0 || searchQuery ? (
-    <>
-      <MediaList
-        FilterFunction={filterFunction}
-        id="bookmarkedMovies"
-        heading="Bookmarked Movies"
-        isBookmarkPage
-      />
-      {!searchQuery && (
-        <MediaList
-          FilterFunction={filterBookmarkedSeries}
-          id="bookmarkedSeries"
-          heading="Bookmarked TV Series"
-          isBookmarkPage
-        />
-      )}
-    </>
-  ) : (
-    <h2 className="title">You have no bookmarked show</h2>
-  );
+  return "test";
+  // return bookmarkedMedia.length > 0 || searchQuery ? (
+  //   <>
+  //     {/* <MediaList
+  //       // FilterFunction={filterFunction}
+  //       id="bookmarkedMovies"
+  //       heading="Bookmarked Movies"
+  //       isBookmarkPage
+  //     />
+  //     {!searchQuery && (
+  //       <MediaList
+  //         // FilterFunction={filterBookmarkedSeries}
+  //         id="bookmarkedSeries"
+  //         heading="Bookmarked TV Series"
+  //         isBookmarkPage
+  //       />
+  //     )} */}
+  //   </>
+  // ) : (
+  //   <h2 className="title">You have no bookmarked show</h2>
+  // );
 };
 
 export default Bookmarks;
